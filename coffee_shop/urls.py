bc85da6dcd93f1740dd56e9f074e9e7bab642f4b
Nestor Fabio Gomez Toro <neyblue15@hotmail.com>
2025-02-17 08:30:58 -0500

    Config form login
    - add crispy to styles login

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("productos/", include("products.urls")),
+    path("usuarios/", include("users.urls")),
 ]
 
 if settings.DEBUG:
